653115b90eda5be015819db5e87869d6c6202e62
DragosDorneanu <dragos.a.dorneanu@gmail.com>
2016-08-27 13:56:54 +0300

    Update MoveException.java

--- customExceptions/MoveException.java
+++ customExceptions/MoveException.java
@@ -5,6 +5,6 @@ public class MoveException extends Exception
 	public static final long serialVersionUID = 0L;
 	@Override
 	public String getMessage(){
-		return "Move exception : to few arguments or invalid move target";
+		return "Move Exception : to few arguments or invalid move target";
 	}
 }
